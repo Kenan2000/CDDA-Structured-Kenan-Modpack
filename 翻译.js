67dee23b76bc8f2614cefc42b35465b58379bcfe
waveyl <514065589@qq.com>
2023-01-20 10:58:13 +0800

    test

--- 翻译.js
+++ 翻译.js
@@ -444,7 +444,7 @@ async function translateWithCache(value, modTranslationCache, context) {
   } else {
     // 没有缓存，就更新缓存
     logger.log(`No Cached Translation for ${value}\n`);
-    translatedValue = await tryTranslation(value);
+    translatedValue = 'x';
     logger.log(`New Translation ${value}\n -> ${translatedValue}\n`);
     modTranslationCache.insertToCache(value, translatedValue);
   }
